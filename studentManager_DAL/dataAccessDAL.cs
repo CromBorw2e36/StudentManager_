28e07378752c638b8e4db228e33d28a668693cdc
Crom Borw <66833938+CromBorw2e36@users.noreply.github.com>
2023-02-22 10:54:55 +0700

    create DAL

---
+++ studentManager_DAL/dataAccessDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentManager_DAL
+{
+    public class dataAccessDAL
+    {
+        public string connectString = "data source=.;initial catalog=QLHV;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+        public bool isConnected = false;
+        public SqlConnection connect = null;
+        public void openConnect()
+        {
+            if (!isConnected)
+            {
+                connect = new SqlConnection(connectString);
+                connect.Open();
+                isConnected = true;
+            }
+        }
+
+        public void closeConnect()
+        {
+            if (isConnected && connect != null)
+            {
+                connect.Close();
+                isConnected = false;
+            }
+        }
+    }
+}
